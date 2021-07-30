1c698c28f397c213bbf92d72a2847e953090eb1f
xiaozuo <xiaozuo1011@163.com>
2021-07-30 20:18:49 +0800

    test2_commit

--- src/main/java/com/xiaozuo/git_test/Demo.java
+++ src/main/java/com/xiaozuo/git_test/Demo.java
@@ -3,5 +3,6 @@ package com.xiaozuo.git_test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Idea_Git");
+        System.out.println("Hello Idea_Git2");
     }
 }
